4c81af53c983ca9af9cc6132e52abeaad4f9cb75
gustavo.lima <@choqueE123>
2026-05-08 17:55:23 -0300

    Aulas 1 e 2

--- aulas_go/aula2/main.go
+++ aulas_go/aula2/main.go
@@ -28,7 +28,6 @@ func Dividir(a, b int) int {
 }
 
 // Retorna File ou nill
-
 func LerArquivoSimples(caminho string) *os.File {
 
 	arquivo, err := os.Open(caminho)
@@ -41,10 +40,13 @@ func LerArquivoSimples(caminho string) *os.File {
 	return arquivo
 }
 
-// mesma função, mas...
-// retorna mais de um valor, sempre (arq, err, tamanho)
-// nomeando os retornos e já aproveitando na atribuição
-
+/*
+mesma funcionalidade de LerArquivoSimples, mas...
+Retorna mais de um valor, sempre (arq, err, tamanho)
+Nomeando os retornos e já aproveitando na atribuição
+E com comentário colado, vira uma doc ao passar o mouse em cima da função!
+Pode ser usado futuramento para gerar uma doc com o comando go doc ...
+*/
 func LerArquivo(caminho string) (arq *os.File, err error, tamanho int) {
 
 	arq, err = os.Open(caminho)
@@ -60,35 +62,40 @@ func LerArquivo(caminho string) (arq *os.File, err error, tamanho int) {
 func main() {
 
 	defer func() {
-		fmt.Println("Func anônima por último por conta do defer")
+		fmt.Println("Func anônima. Por último por conta do defer")
 	}()
 
-	fmt.Println("Olá")
+	fmt.Println("Olá ------------------")
 	fmt.Println(Somar(5, 10))
 	fmt.Println(Dividir(45, 0))
 
 	arq, err, _ := LerArquivo("arquivo.csv")
-	// ignorei a variável tamanho do retorno
+	// ignorei a variável tamanho
 
 	if err != nil {
 		fmt.Println("Erro ao abrir arquivo", err)
 		return // Para parar o fluxo
 	}
+
 	defer arq.Close()
 	// defer executa ao terminar a função (no caso a main)
 	// Close executa antes da func anônima pq defer executa de baixo para cima
 
 	// GO já tem o garbage collector que limpa a memória com o programa aberto
 	// mas é bom impar a memória para não precisar esperar ele limpar sozinho
-	// Ex. leio um arquivo grande e guardo em memória, fecho o arquivo, e só depois de editar o conteúdo. eu devolvo para o arquivo (abro de novo)
-	// Assim que inicar uma conexão, já posso utilizar o defer para fechar, ai não esqueço.
+	// Ex. leio um arquivo grande e guardo em memória, fecho o arquivo, e só depois de editar o conteúdo. eu devolvo para o arquivo (abro de novo?)
+	// Ex. tilizar o defer para fechar conexão.
 
 	nome := "Gustavo"
 
 	defer func(nomeRecebido string) {
 		fmt.Println(nomeRecebido + " | " + nome)
-	}(nome) // mesmo com defer, já Gravou Gustavo, não adianta mudar
+	}(nome) // mesmo com defer, já gravou Gustavo, não adianta mudar
 
 	nome = "Lima"
 
 }
+
+// defer é util para log, mandar email, etc
+// recovery da aplicação também, ex:
+// app caiu, e tento subir de novo, ou mando um email.
